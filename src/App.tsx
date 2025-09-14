fef09d3ae1f572bfb4efb15f79dde9c06a92b499
saipranav1234 <saipranav9912345@gmail.com>
2025-09-14 16:58:59 +0530

    demo

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className='text-3xl font-bold underline text-center' >HI how are you </h1>
+      <h1 className='text-3xl font-bold underline text-center' >HI how are  </h1>
     </div>
   )
 }
